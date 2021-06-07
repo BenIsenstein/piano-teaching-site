82720beb8ec7a2693cff7b948e8599b455a5dfe0
Ben Isenstein <ben.isenstein@gmail.com>
2021-06-07 12:57:41 -0600

    emptied client server

--- client/src/components/AgentOnly.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import AuthenticationContext from "../AuthenticationContext"
-
-const AgentOnly = ({children}) => {
-    const authContext = useContext(AuthenticationContext)
-    return (authContext.isAgent) ? children : null 
-}
-
-export default AgentOnly
